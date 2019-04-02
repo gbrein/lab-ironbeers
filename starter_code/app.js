c00cc01df0628cac40cdb1c1ae8a33369e9a52f9
gui <gbrein@gmail.com>
2019-04-02 16:04:09 -0300

    index, layaout e beer .hbs terminados

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -15,7 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
 
+  punkAPI.getBeers()
+    .then(beers => {
+    });
+    .catch(error => {
+      console.log(error)
+    });
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 
 app.listen(3000);
